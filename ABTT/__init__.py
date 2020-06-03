1075eab5f787778136fb052dd763b8c44dde4ed3
Alister Burt <alisterburt@gmail.com>
2020-06-03 14:50:39 +0200

    updated init

--- ABTT/__init__.py
+++ ABTT/__init__.py
@@ -1,5 +1,4 @@
 from .fsc import relion
-from .io import mrc, star, pdb
-from .local_resolution import spatial
+from .io import mrc, star  # pdb
+# from .local_resolution import spatial
 from .math import rotate3d, euler_angles, spatial_frequency
-
